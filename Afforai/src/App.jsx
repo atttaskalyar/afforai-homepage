13588dc65c07b5cbb1a43f37c1ab232824101671
atttaskalyar <attaskalyar@gmail.com>
2023-12-09 17:27:06 +0500

    initialized repository

---
+++ Afforai/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+import Navbar from './components/UseCaseComponents/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+     </>
+  )
+}
+
+export default App
---
+++ Afforai/src/components/UseCaseComponents/Navbar/index.jsx
@@ -0,0 +1,13 @@
+const  Navbar =() =>{
+  return (
+   <>
+   <div>
+    <nav className="py-0 py-md-4 px-md-5 flex-column navbar navbar-expand-md navbar-light fixed-top bg-black">
+
+    </nav>
+   </div>
+   </>
+  );
+}
+
+export default Navbar;
