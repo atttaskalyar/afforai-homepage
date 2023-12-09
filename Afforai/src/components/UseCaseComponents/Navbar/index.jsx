973ebdba2c77718176b6dec499b6195d70015662
atttaskalyar <attaskalyar@gmail.com>
2023-12-09 23:50:55 +0500

    implemented changes

--- Afforai/src/components/UseCaseComponents/Navbar/index.jsx
+++ Afforai/src/components/UseCaseComponents/Navbar/index.jsx
@@ -1,9 +1,22 @@
-const  Navbar =() =>{
+const  Navbar =(isMobile) =>{
   return (
    <>
    <div>
-    <nav className="py-0 py-md-4 px-md-5 flex-column navbar navbar-expand-md navbar-light fixed-top bg-black">
+    <nav className="py-0 md:py-4 md:px-5 flex-column items-center">
+    <div className="flex flex-row items-center px-6 py-2">
+        <div className="w-100 flex justify-between">
+            <a className="d-flex flex-shrink-1 justify-content-start align-items-center me-0 py-0 navbar-brand" href="/">
+                Afforai
+            </a>
+        <div>
 
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
+        </div>
+        </div>
+        
+    </div>
     </nav>
    </div>
    </>
