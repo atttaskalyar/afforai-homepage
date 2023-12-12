217da1d83aba8ac7ea840c403df5ce6d341ba74b
anashanif1999 kalyar <attaskalyar@gmail.com>
2023-12-12 23:30:10 +0500

    implementing carousel. Saving due to electricity fluctuations

---
+++ Afforai/src/components/UseCaseComponents/DraggableMovingSlider/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./style.module.css";
+
+const DraggableMovingSlider = ({[elements]}) => {
+ <div className={styles.carouselContainer}>
+    <div className={styles.carouselTrack}>
+    
+    </div>
+
+ </div>
+};
+
+export default DraggableMovingSlider;
