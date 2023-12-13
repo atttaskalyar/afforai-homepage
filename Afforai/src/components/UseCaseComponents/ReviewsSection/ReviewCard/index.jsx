013b065226370065fed071f4ce140a39847e1b95
anashanif1999 kalyar <attaskalyar@gmail.com>
2023-12-14 01:16:23 +0500

    added profile

--- Afforai/src/components/UseCaseComponents/ReviewsSection/ReviewCard/index.jsx
+++ Afforai/src/components/UseCaseComponents/ReviewsSection/ReviewCard/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./style.module.css";
+import Image from "./../../../BaseComponents/Images/Image";
 
 const ReviewCard = ({ style }) => {
   return (
@@ -7,7 +8,16 @@ const ReviewCard = ({ style }) => {
         Placeholder text for the review. This is placeholder text that can be
         used for this review card
       </div>
-      <div className={styles.cardAuthorContainer}></div>
+      <div className={styles.cardAuthorContainer}>
+        <div className={styles.cardAuthorDetailsContainer}>
+          <Image
+            src="public/images/ProfilePictures/profile.webp"
+            className={styles.authorPicture}
+          />
+          <div>Name</div>
+        </div>
+        <div className={styles.productHunt}></div>
+      </div>
     </div>
   );
 };
